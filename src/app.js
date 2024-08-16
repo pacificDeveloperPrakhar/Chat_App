4bfa634c348c4ab35744f45e54f135e25dbb295b
prakhar vishwakarma <prakharvision@gmail.com>
2024-08-16 20:20:49 +0530

    Implement session storage using MongoDB Atlas with custom options- Stored sessions in MongoDB Atlas using connect-mongo for enhanced scalability and reliability.- Configured session options with `ttl` for auto-removal of sessions after 14 days and `autoRemove: 'native'` for efficient cleanup of expired sessions.- Added custom cookie settings to control expiration and security, ensuring cookies last 14 days with a `maxAge` property.- This approach offers better persistence, automatic session management, and flexibility, especially useful in distributed environments.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express=require("express")
 const profileRoutes=require("./routes/profileRoutes.js")
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const app=express()
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,                      
+    saveUninitialized: true,           // will create the session once it is modified
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,  // MongoDB connection URL
+      collectionName: 'sessions',        // Custom collection name
+      ttl: 14 * 24 * 60 * 60,           // will remove the session after 14 days
+      autoRemove: 'native'              // this will remove the expired session automatically
+    }),
+    cookie: {
+      secure: false,                    // Set to true if using HTTPS
+      maxAge: 14 * 24 * 60 * 60 * 1000  // cookie expiration time is 14 days
+    }
+  }));
 app.use(express.json())
 app.use("/profiles",profileRoutes)
 module.exports=app
