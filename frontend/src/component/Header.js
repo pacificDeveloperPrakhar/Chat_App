cbe156d28fbe8a8f81f3a6f0e3ffb19adf34cea9
prakhar vishwakarma <prakharvision@gmail.com>
2024-08-19 00:22:27 +0530

    i have added the icons

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { usersConnectionModify } from "../slices/userSlice";
 import io from "socket.io-client";
 import Avatar from '@mui/material/Avatar';
+import { FaInfoCircle, FaComments, FaSignInAlt, FaUser } from 'react-icons/fa'; // Import suitable icons
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -30,28 +31,29 @@ const Header = () => {
 
   return (
     <nav
-      className="bg-orange-500 shadow-2xl h-full w-64 fixed left-0 top-0 flex flex-col justify-between text-white font-semibold"
+      className="bg-orange-500 shadow-lg box-content h-full w-64 fixed left-0 top-0 flex flex-col justify-between text-white font-semibold"
       style={{ boxSizing: 'border-box' }}
     >
-      <div>
-        <div className="h-16 flex items-center justify-center bg-orange-600 w-full shadow-md">
-          <h2 className="text-xl">ChatBar</h2>
-        </div>
-        <ul className="flex flex-col justify-start items-start h-auto w-full px-4 space-y-4 pt-4">
-          <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full">
-            <Link to="about">About</Link>
-          </li>
-          <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full">
-            <Link to="chat">Chat</Link>
-          </li>
-          <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full">
-            <Link to="form">Sign in</Link>
-          </li>
-        </ul>
+      <div className="h-16 flex items-center justify-center bg-orange-600 w-full shadow-md">
+        <h2 className="text-xl">ChatBar</h2>
       </div>
-      
-      <div className="flex items-center justify-start w-full px-4 py-4 space-x-2 bg-orange-600 shadow-inner">
+      <ul className="flex flex-col justify-start items-start h-full w-full px-4 space-y-4 pt-4">
+        <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full flex items-center space-x-2">
+          <FaInfoCircle />
+          <Link to="about">About</Link>
+        </li>
+        <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full flex items-center space-x-2">
+          <FaComments />
+          <Link to="chat">Chat</Link>
+        </li>
+        <li className="hover:bg-orange-600 px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer w-full flex items-center space-x-2">
+          <FaSignInAlt />
+          <Link to="form">Sign in</Link>
+        </li>
+      </ul>
+      <div className="mb-4 px-4 flex items-center space-x-2">
         <Avatar alt={user.username} src={user.profileUrl} />
+        <FaUser />
         <Link to="dashboard">{user.username}</Link>
       </div>
     </nav>
