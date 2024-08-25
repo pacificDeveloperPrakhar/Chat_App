e867db8bb134ad9ee74360411e327c0290b8b66d
prakhar vishwakarma <prakharvision@gmail.com>
2024-08-25 13:34:17 +0530

    i have addded the chat filtering sorting it by send date and then also added the pagination for fetching the chats when users scroll to the top of the chat

--- src/controllers/chatControllers.js
+++ src/controllers/chatControllers.js
@@ -2,7 +2,7 @@ const catchAsync = require('../utils/catchAsync.js');
 const AppError=require("../utils/appErrors.js")
 const {db}=require('../db/db_connection.js')
 const {message,conversations,users}=require("../db/schema/schema.js");
-const { eq } = require('drizzle-orm');
+const { eq,asc, desc } = require('drizzle-orm');
 const appError = require('../utils/appErrors.js');
 exports.getAllChatsInAConversation=catchAsync(async function(req,res,next){
     console.log("chat route hit")
@@ -13,7 +13,7 @@ exports.getAllChatsInAConversation=catchAsync(async function(req,res,next){
    conversation=db.select().from(conversations).where(eq(conversations.id,conversationId))
    if(!conversation)
    return next(new appError("this conversation does exist in the database",400));
-   messages=await db.select().from(message).where(eq(message.id,conversationId))
+   messages=await db.select().from(message).where(eq(message.id,conversationId)).orderBy(desc(message.sendAt)).limit(limit).offset(limit*(page-1))
    const length=message.length
    res.status(200).json({
     status:"ok",
